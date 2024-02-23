f011e588979d6ea4c0b87df106c0e3bf407729eb
neminsheth <neminsheth24@gmail.com>
2024-02-23 22:37:05 +0530

    Scrollview for questionnaire.dart

--- lib/questionnaire/questionnaire.dart
+++ lib/questionnaire/questionnaire.dart
@@ -50,24 +50,36 @@ class _QuestionnairePageState extends State<QuestionnairePage> {
                   _buildQuestion('Your current class level is:', ['Freshman', 'Sophomore', 'Junior', 'Senior', 'Graduate student', 'Other']),
                   _buildQuestion('Your gender is:', ['Female', 'Male', 'Other / I prefer not to respond']),
                   _buildQuestionWithSlider('How stressed do you feel on a daily basis during the academic year?'),
-                  _buildMultiChoiceQuestion('What are the usual causes of stress in your life?', [
-                    'Studies issues', 'Financial issues', 'Family issues', 'Friends issues', 'Issues with the significant other (partner)', 'Work (job-related) issues', 'Health Related Issues', 'Sports / Athletics activities issues', 'My involvement in clubs and organizations', 'Other',
-                  ]),
-                  _buildMultiChoiceQuestion('What are the usual BEHAVIORAL effects of stress you\'ve noticed at yourself?', [
+                  SingleChildScrollView(
+                    child: _buildMultiChoiceQuestion('What are the usual causes of stress in your life?', [
+                      'Studies issues', 'Financial issues', 'Family issues', 'Friends issues', 'Issues with the significant other (partner)', 'Work (job-related) issues', 'Health Related Issues', 'Sports / Athletics activities issues', 'My involvement in clubs and organizations', 'Other',
+                    ]),
+                  ),
+                  SingleChildScrollView
+                    (child:_buildMultiChoiceQuestion('What are the usual BEHAVIORAL effects of stress you\'ve noticed at yourself?', [
                     'Change in activity levels', 'Decreased efficiency and effectiveness', 'Difficulty communicating', 'Increased sense of humor/gallows humor', 'Irritability, outbursts of anger, frequent arguments', 'Inability to rest, relax or let down', 'Change in eating habits', 'Change in sleep patterns', 'Change in activity performance', 'Periods of crying', 'Increased use of tobacco, alcohol, drugs, sugar or caffeine', 'Hyper-vigilance about safety or the surrounding environment', 'Avoidance of activities or places that trigger memories', 'Accident prone', 'Other',
                   ]),
-                  _buildMultiChoiceQuestion('What are the usual PSYCHOLOGICAL or EMOTIONAL effects of stress you\'ve noticed at yourself?', [
-                    'Feeling heroic, euphoric or invulnerable', 'Denial', 'Anxiety or fear', 'Worry about safety of self or others', 'Irritability or anger', 'Restlessness', 'Sadness, moodiness, grief or depression', 'Vivid or distressing dreams', 'Guilt or "survivor guilt"', 'Feeling overwhelmed, helpless or hopeless', 'Feeling isolated, lost, lonely or abandoned', 'Apathy', 'Over-identification with survivors', 'Feeling misunderstood or unappreciated', 'None of the Above', 'Other',
-                  ]),
-                  _buildMultiChoiceQuestion('What are the usual COGNITIVE effects of stress you\'ve noticed at yourself?', [
-                    'Memory problems/forgetfulness', 'Disorientation', 'Confusion', 'Slowness in thinking, analyzing, or comprehending', 'Difficulty calculating, setting priorities or making decisions', 'Difficulty Concentrating', 'Limited attention span', 'Loss of objectivity', 'Inability to stop thinking about the disaster or an incident', 'None of the Above', 'Other',
-                  ]),
-                  _buildMultiChoiceQuestion('What are the usual SOCIAL effects of stress you\'ve noticed at yourself?', [
-                    'Withdrawing or isolating from people', 'Difficulty listening', 'Difficulty sharing ideas', 'Difficulty engaging in mutual problem solving', 'Blaming', 'Criticizing', 'Intolerance of group process', 'Difficulty in giving or accepting support or help', 'Impatient with or disrespectful to others', 'None of the Above', 'Other',
-                  ]),
-                  _buildMultiChoiceQuestion('What are your personal methods to relieve stress?', [
-                    'Eating', 'Sleeping', 'Drinking', 'Drugs', 'Sports / Exercise', 'Talking with someone', 'Shopping', 'Computer Games', 'Social Media', 'None of the Above', 'Other',
-                  ]),
+                  ),
+                  SingleChildScrollView(
+                    child: _buildMultiChoiceQuestion('What are the usual PSYCHOLOGICAL or EMOTIONAL effects of stress you\'ve noticed at yourself?', [
+                      'Feeling heroic, euphoric or invulnerable', 'Denial', 'Anxiety or fear', 'Worry about safety of self or others', 'Irritability or anger', 'Restlessness', 'Sadness, moodiness, grief or depression', 'Vivid or distressing dreams', 'Guilt or "survivor guilt"', 'Feeling overwhelmed, helpless or hopeless', 'Feeling isolated, lost, lonely or abandoned', 'Apathy', 'Over-identification with survivors', 'Feeling misunderstood or unappreciated', 'None of the Above', 'Other',
+                    ]),
+                  ),
+                  SingleChildScrollView(
+                    child: _buildMultiChoiceQuestion('What are the usual COGNITIVE effects of stress you\'ve noticed at yourself?', [
+                      'Memory problems/forgetfulness', 'Disorientation', 'Confusion', 'Slowness in thinking, analyzing, or comprehending', 'Difficulty calculating, setting priorities or making decisions', 'Difficulty Concentrating', 'Limited attention span', 'Loss of objectivity', 'Inability to stop thinking about the disaster or an incident', 'None of the Above', 'Other',
+                    ]),
+                  ),
+                  SingleChildScrollView(
+                    child:_buildMultiChoiceQuestion('What are the usual SOCIAL effects of stress you\'ve noticed at yourself?', [
+                      'Withdrawing or isolating from people', 'Difficulty listening', 'Difficulty sharing ideas', 'Difficulty engaging in mutual problem solving', 'Blaming', 'Criticizing', 'Intolerance of group process', 'Difficulty in giving or accepting support or help', 'Impatient with or disrespectful to others', 'None of the Above', 'Other',
+                    ]),
+                  ),
+                  SingleChildScrollView(
+                    child: _buildMultiChoiceQuestion('What are your personal methods to relieve stress?', [
+                      'Eating', 'Sleeping', 'Drinking', 'Drugs', 'Sports / Exercise', 'Talking with someone', 'Shopping', 'Computer Games', 'Social Media', 'None of the Above', 'Other',
+                    ]),
+                  ),
                   _buildQuestionWithSlider('How able do you feel to handle stress when you are experiencing it?'),
                   _buildMultiChoiceQuestion('What are the most pressing stress factors in your current academic context (related to this program of study)?', [
                     'Study workload', 'Grades', 'Financial pressure (e.g. tuition, living costs)', 'Work (and Study) - Life balance', 'Relationship with (some) faculty members', 'Relationship with other students', 'Campus social life', 'Other',
